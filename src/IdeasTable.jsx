b629f29aa0b773917c8033f3fe1d913a76430bcb
taquy <quyt@unifiedpost.com>
2021-04-22 19:18:13 +0700

    add row click

--- src/IdeasTable.jsx
+++ src/IdeasTable.jsx
@@ -1,35 +1,63 @@
-import React from 'react';
-import axios from 'axios';
-import { Table, Input, Button, notification } from 'antd';
-import 'antd/dist/antd.css';
+import React from "react";
+import axios from "axios";
+import { Table, Input, Button, notification } from "antd";
+import "antd/dist/antd.css";
 
 const columns = [
   {
-    title: 'id',
-    dataIndex: 'id',
-    key: 'id'
+    title: "id",
+    dataIndex: "id",
+    key: "id",
   },
   {
-    title: 'idea',
-    dataIndex: 'idea',
-    key: 'idea'
+    title: "idea",
+    dataIndex: "idea",
+    key: "idea",
   },
   {
-    title: 'uploadDate',
-    dataIndex: 'uploadDate',
-    key: 'uploadDate'
-  }
+    title: "uploadDate",
+    dataIndex: "uploadDate",
+    key: "uploadDate",
+  },
 ];
 
 class IdeasTable extends React.Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      ideaInput: '',
+      ideaInput: "",
       ideaSubmitting: false,
-      ideasArray: []
+      ideasArray: [],
     };
   }
+
+  onClickIdea = (event) => {
+    const { idea } = event;
+    axios
+      .get(`/ideaProof/${idea}`)
+      .then((response) => {
+        const { proofs } = response.data;
+        if (proofs && proofs[0] && proofs[0].status) {
+          notification.open({
+            message: "Idea Proof",
+            description: `This idea is ${
+              proofs[0].status
+            } as part of proof starting with ${proofs[0].versionProofId.substr(
+              0,
+              5
+            )}.`,
+          });
+        } else {
+          notification.open({
+            message: "Failed to get Proof.",
+            description: "Could not get the proof for this idea!",
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   componentDidMount() {
     this.fetchIdeas();
@@ -37,47 +65,47 @@ class IdeasTable extends React.Component<Props, State> {
 
   fetchIdeas = () => {
     axios
-      .get('/ideas')
-      .then(response => {
-        console.log('Result of getting ideas:');
+      .get("/ideas")
+      .then((response) => {
+        console.log("Result of getting ideas:");
         console.log(response);
         this.setState({ ideasArray: response.data });
       })
-      .catch(err => {
-        console.error('Failed to get ideas with err');
+      .catch((err) => {
+        console.error("Failed to get ideas with err");
         console.error(err);
       });
   };
 
-  slugify = string => {
-    const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;';
-    const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------';
-    const p = new RegExp(a.split('').join('|'), 'g');
+  slugify = (string) => {
+    const a = "àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;";
+    const b = "aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------";
+    const p = new RegExp(a.split("").join("|"), "g");
     return string
       .toString()
       .toLowerCase()
-      .replace(/\s+/g, '-') // Replace spaces with -
-      .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-      .replace(/&/g, '-and-') // Replace & with ‘and’
-      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-      .replace(/\-\-+/g, '-') // Replace multiple - with single -
-      .replace(/^-+/, '') // Trim - from start of text
-      .replace(/-+$/, ''); // Trim - from end of text
+      .replace(/\s+/g, "-") // Replace spaces with -
+      .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, "-and-") // Replace & with ‘and’
+      .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+      .replace(/\-\-+/g, "-") // Replace multiple - with single -
+      .replace(/^-+/, "") // Trim - from start of text
+      .replace(/-+$/, ""); // Trim - from end of text
   };
 
   onClickSubmit = () => {
     const { ideaInput } = this.state;
     this.setState({ ideaSubmitting: true });
-    this.setState({ ideaInput: '' });
+    this.setState({ ideaInput: "" });
     axios
       .get(`/proveIt/${this.slugify(ideaInput)}`)
-      .then(response => {
-        console.log('Submitted new idea.');
+      .then((response) => {
+        console.log("Submitted new idea.");
         this.setState({ ideaSubmitting: false });
         this.fetchIdeas();
       })
-      .catch(err => {
-        console.error('Failed to submit with err');
+      .catch((err) => {
+        console.error("Failed to submit with err");
         this.setState({ ideaSubmitting: false });
         console.error(err);
       });
@@ -91,7 +119,7 @@ class IdeasTable extends React.Component<Props, State> {
           placeholder="Add an idea!"
           value={ideaInput}
           onPressEnter={this.onClickSubmit}
-          onChange={e => {
+          onChange={(e) => {
             this.setState({ ideaInput: e.target.value });
           }}
         />
@@ -99,7 +127,7 @@ class IdeasTable extends React.Component<Props, State> {
           type="primary"
           loading={ideaSubmitting}
           onClick={this.onClickSubmit}
-          style={{ margin: '10px' }}
+          style={{ margin: "10px" }}
         >
           Submit
         </Button>
@@ -109,13 +137,13 @@ class IdeasTable extends React.Component<Props, State> {
             columns={columns}
             onRow={(record, rowIndex) => {
               return {
-                onClick: event => {
-                  console.log('We will add some proof logic here later.');
+                onClick: (event) => {
+                  this.onClickIdea(record);
                 }, // click row
-                onDoubleClick: event => {}, // double click row
-                onContextMenu: event => {}, // right button click row
-                onMouseEnter: event => {}, // mouse enter row
-                onMouseLeave: event => {} // mouse leave row
+                onDoubleClick: (event) => {}, // double click row
+                onContextMenu: (event) => {}, // right button click row
+                onMouseEnter: (event) => {}, // mouse enter row
+                onMouseLeave: (event) => {}, // mouse leave row
               };
             }}
           />
